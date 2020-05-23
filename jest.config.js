29c81f8688c443f43a046ce3861f7e20216d564b
Ashish Shetty <ashishshetty777@gmail.com>
2020-05-23 17:52:35 +0530

    Coverage threshold added

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,18 @@ module.exports = {
     '**/pages/**/*.js',
     '**/utils/**/*.js',
   ],
+  coverageThreshold: {
+    global: {
+      statements: 30,
+      branches: 30,
+      functions: 25, // this gets down because we removed utils from global threshold
+      lines: 30,
+    },
+    './utils': {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
