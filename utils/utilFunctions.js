1d18dcc20bbfa00ec1367fa57be34766118a378a
Ashish Shetty <ashishshetty777@gmail.com>
2020-05-23 22:47:21 +0530

    Testing jest --findRelatedTests

--- utils/utilFunctions.js
+++ utils/utilFunctions.js
@@ -1,6 +1,7 @@
 export const getRandomNumber = () => {
   return 2
 }
+// adding comment for testing "jest --findRelatedTests"
 
 export const returnEvenNumbers = () => {
   const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12]
